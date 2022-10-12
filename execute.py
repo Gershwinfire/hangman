23c8d7a1188a96d95c6332cea951a3bb8014a3c7
Gershwinfire <94252760+Gershwinfire@users.noreply.github.com>
2022-10-12 13:31:56 -0400

    removed one populate_board_letters()

--- execute.py
+++ execute.py
@@ -11,15 +11,12 @@ game_board = Board(player1, game_word, game_letters)
 
 while True:
 
-    game_board.game_letters.populate_board_letters()
     game_board.game_letters.print_board_letters()
     display_word = game_board.display_game_word()
     if '_' not in display_word:
         print("CONGRATULATIONS")
         break
-    game_board.guess_letter()
+    game_board.compare_letter(game_board.guess_letter())
     if game_board.wrong_guesses >= 6:
         print(f"MAN WAS HANGED!\nGAME OVER\nGame Word: {game_word.word}")
         break
-    
-   
